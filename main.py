8ec019fab8ba12ad2e155c3a801d06eedf37159e
yzimmermann <92678727+yzimmermann@users.noreply.github.com>
2023-01-03 23:21:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ consumer_key = sys.argv[1]
 consumer_secret = sys.argv[2]
 access_token = sys.argv[3]
 access_token_secret = sys.argv[4]
-openai_api_key = sys.argv[5]
+openai.api_key = sys.argv[5]
 
 auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
 api = tweepy.API(auth)
