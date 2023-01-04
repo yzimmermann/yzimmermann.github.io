43046dc42bb5a301be3c6f9dde5a50f4d247c734
yzimmermann <92678727+yzimmermann@users.noreply.github.com>
2023-01-04 10:07:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,21 @@
 import openai
 import markdown
-import tweepy
 import sys
 import pytrends
-import os
+
 
 #Initialize pytrends
 from pytrends.request import TrendReq
 pytrends = TrendReq(hl='en-US', tz=360)
 
 
-# Authenticate with the Twitter API
-consumer_key = sys.argv[1]
-consumer_secret = sys.argv[2]
-access_token = sys.argv[3]
-access_token_secret = sys.argv[4]
-openai.api_key = sys.argv[5]
+# Authenticate with the OpenAI API
+openai.api_key = sys.argv[1]
 
-auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-api = tweepy.API(auth)
 
 # Fetch the top trending topic
 trends_response = pytrends.trending_searches()
 top_trend = trends_response[0][0]
-os.environ["trend_top"] = top_trend
 
 # Use the OpenAI API to generate a piece of text on the top Twitter trend
 prompt = f"Write a comprehensive article that is at least 500 words long on the topic: {top_trend}"
